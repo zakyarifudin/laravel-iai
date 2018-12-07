ac100454af8fd73e2e8ed2fa3f8696f0394f80c3
zakyarifudin <zakyarifudin9@gmail.com>
2018-12-07 09:03:40 +0700

    tambah middleware is not login

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'islogin'   => \App\Http\Middleware\IsLogin::class,
+        'isnotlogin'   => \App\Http\Middleware\IsNotLogin::class,
     ];
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -5,24 +5,24 @@ Route::get('/', function () {
 });
 
 //login
-Route::get('/login', 'UserController@viewLogin')->name('login');
+Route::get('/login', 'UserController@viewLogin')->name('login')->middleware('isnotlogin');
 Route::post('/login', 'UserController@postLogin');
 
 //logout
-Route::get('/logout', 'UserController@logout')->name('logout');
+Route::get('/logout', 'UserController@logout')->name('logout')->middleware('islogin');
 
 // register
-Route::get('/register', 'UserController@viewRegister')->name('register');
+Route::get('/register', 'UserController@viewRegister')->name('register')->middleware('isnotlogin');
 Route::post('/register', 'UserController@postRegister');
 
 // question
-Route::get('/question', 'QuestionController@index')->name('question');
-Route::get('/question/{id}', 'QuestionController@show')->name('question.show');
-Route::post('/question/store', 'QuestionController@store')->name('question.store');
-Route::put('/question/update/{id}', 'QuestionController@update')->name('question.update');
-Route::delete('/question/delete/{id}', 'QuestionController@destroy')->name('question.delete');
+Route::get('/question', 'QuestionController@index')->name('question')->middleware('islogin');
+Route::get('/question/{id}', 'QuestionController@show')->name('question.show')->middleware('islogin');
+Route::post('/question/store', 'QuestionController@store')->name('question.store')->middleware('islogin');
+Route::put('/question/update/{id}', 'QuestionController@update')->name('question.update')->middleware('islogin');
+Route::delete('/question/delete/{id}', 'QuestionController@destroy')->name('question.delete')->middleware('islogin');
 
 // comment
-Route::post('/question/{id}/comment/store', 'CommentController@store')->name('comment.store');
-Route::put('/question/comment/update/{id}', 'CommentController@update')->name('comment.update');
-Route::delete('/question/comment/delete/{id}', 'CommentController@destroy')->name('comment.delete');
+Route::post('/question/{id}/comment/store', 'CommentController@store')->name('comment.store')->middleware('islogin');
+Route::put('/question/comment/update/{id}', 'CommentController@update')->name('comment.update')->middleware('islogin');
+Route::delete('/question/comment/delete/{id}', 'CommentController@destroy')->name('comment.delete')->middleware('islogin');
